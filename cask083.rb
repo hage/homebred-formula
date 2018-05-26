a71b8188c420a2d0c541a7a067d617aa7fee0ec0
URA Takefumi <ura.takefumi@gmail.com>
2018-05-26 18:04:53 +0900

    import formula of cask 0.8.3

---
+++ cask083.rb
@@ -0,0 +1,32 @@
+class Cask < Formula
+  desc "Emacs dependency management"
+  homepage "https://cask.readthedocs.org/"
+  url "https://github.com/cask/cask/archive/v0.8.3.tar.gz"
+  sha256 "71bafe94b7ea08b0f9075df2cb3cc2063a00c88e64fb1bf7073ae1b5fa2560cc"
+  head "https://github.com/cask/cask.git"
+
+  bottle :unneeded
+
+  depends_on "emacs"
+
+  def install
+    bin.install "bin/cask"
+    prefix.install "templates"
+    # Lisp files must stay here: https://github.com/cask/cask/issues/305
+    prefix.install Dir["*.el"]
+    elisp.install_symlink "#{prefix}/cask.el"
+    elisp.install_symlink "#{prefix}/cask-bootstrap.el"
+
+    # Stop cask performing self-upgrades.
+    touch prefix/".no-upgrade"
+  end
+
+  test do
+    (testpath/"Cask").write <<~EOS
+      (source gnu)
+      (depends-on "chess")
+    EOS
+    system bin/"cask", "install"
+    (testpath/".cask").directory?
+  end
+end
